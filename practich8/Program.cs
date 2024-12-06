a71a1466939a49c3757e7c7b80e192d5c259ef38
ИСП-22 <-22@LAB30-14>
2024-12-06 14:03:28 +0300

    Добавьте файлы проекта.

---
+++ practich8/Program.cs
@@ -0,0 +1,10 @@
+﻿{
+    for (int i = 1; i <= 9; i++)
+    {
+        for (int j = 1; j <= 9; j++)
+        {
+            Console.Write($"{j} x {i} = {i * j}\t");
+        }
+        Console.WriteLine();
+    }
+}
